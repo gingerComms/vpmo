4df02d56493fe06b304a23138f7e16d640984e20
Abhishek Verma <wigeriaaeriag@gmail.com>
2018-09-01 11:06:46 +0530

    Switching to prod db

--- vpmoprj/settings.py
+++ vpmoprj/settings.py
@@ -130,7 +130,7 @@ WSGI_APPLICATION = 'vpmoprj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-if DEBUG:
+if False and DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "djongo",
