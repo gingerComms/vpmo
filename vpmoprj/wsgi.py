f2536a36a000c0969c8289b8c1a9b15dd7940720
Abhishek Verma <wigeriaaeriag@gmail.com>
2018-11-26 14:18:16 +0530

    Wsgi fix

--- vpmoprj/wsgi.py
+++ vpmoprj/wsgi.py
@@ -14,19 +14,41 @@ sys.path.append("/home/ubuntu/vpmo")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vpmoprj.settings")
 
+from django.core.wsgi import get_wsgi_application
 from django.core.handlers.wsgi import WSGIHandler
+
+KEYS_TO_LOAD = ["TWILIO_CHAT_SERVICE_SID", "TWILIO_ACCOUNT_SID", "TWILIO_API_KEY", "TWILIO_SECRET_KEY", "VPMO_TWILIO_AUTH_TOKEN", "VPMO_AWS_ACCESS", "VPMO_AWS_SECRET", "MONGO_HOST"]
+
+
+def loading_app(wsgi_environ, start_response):
+    global real_app
+    import os
+    for key in KEYS_TO_LOAD:
+        try:
+            os.environ[key] = wsgi_environ[key]
+        except KeyError:
+            # The WSGI environment doesn't have the key
+            pass
+    from django.core.wsgi import get_wsgi_application
+    real_app = get_wsgi_application()
+    return real_app(wsgi_environ, start_response)
+
+real_app = loading_app
+
+application = lambda env, start: real_app(env, start)
+
+"""
 
 class WSGIEnvironment(WSGIHandler):
 
     def __call__(self, environ, start_response):
 
-        env_variables = ["TWILIO_CHAT_SERVICE_SID", "TWILIO_ACCOUNT_SID", "TWILIO_API_KEY", "TWILIO_SECRET_KEY", "VPMO_TWILIO_AUTH_TOKEN", "VPMO_AWS_ACCESS", "VPMO_AWS_SECRET", "MONGO_HOST"]
-
+        
         for variable in env_variables:
             os.environ[variable] = environ[variable]
         return super(WSGIEnvironment, self).__call__(environ, start_response)
 
 application = WSGIEnvironment()
+"""
 
-# from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
