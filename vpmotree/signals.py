9a091397e98c03abbfd436840a1b320c9593331c
Abhishek Verma <wigeriaaeriag@gmail.com>
2018-12-05 10:50:08 +0530

    Adding project post save signal for task lists

--- vpmotree/signals.py
+++ vpmotree/signals.py
@@ -1,7 +1,17 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from vpmotree.models import *
+from vpmotask.models import ScrumboardTaskList
 
 @receiver(pre_delete, sender=TreeStructure, dispatch_uid='treestructure_delete_signal')
 def delete_node_channel(sender, instance, using, **kwargs):
 	instance.delete_channel()
+
+
+@receiver(post_save, sender=Project, dispatch_uid="project_post_save_signal")
+def create_project_list(sender, instance, *args, **kwargs):
+	""" Creates a task list if a project has no lists - to prevent errors during task creation """
+	existing_task_lists = ScrumboardTaskList.objects.filter(project=instance).first()
+	if existing_task_lists.first() is None:
+		created_task_list = ScrumboardTaskList(project=instance, title="Default List")
+		created_task_list.save()
